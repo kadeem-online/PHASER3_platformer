bdfd302b8c780ba85ecb820107f714e70adf475f
kadeem254 <kadeemalexander254@gmail.com>
2024-10-16 13:48:21 +0300

    DOCS: comments update for test_level_01.ts

--- src/scenes/test-level-01.ts
+++ src/scenes/test-level-01.ts
@@ -1,4 +1,13 @@
+/**
+ *
+ * test-level-01.ts: This scene is for use to test basic aspects of the gameplay.
+ *
+ */
+
+// phaser
 import Phaser from "phaser";
+
+// utilities
 import { SCENES } from "../util/variables";
 
 export default class TestLevel01 extends Phaser.Scene {
@@ -10,8 +19,11 @@ export default class TestLevel01 extends Phaser.Scene {
 		this.__DEBUG_banner();
 	}
 
+	/**
+	 * Debug function to show that we are on the test_level_01 scene using a text
+	 * banner.
+	 */
 	__DEBUG_banner() {
-		// confirm the scene is viewable
 		const debug_text = this.add.text(
 			Number(this.game.config.width) / 2,
 			Number(this.game.config.height) / 2,
