43e67cfb7c129c7f4939693148b8ebb6683c67cd
kadeem254 <kadeemalexander254@gmail.com>
2024-10-16 13:53:57 +0300

    DELETE: removed the default textures from the template and reference in the boot scene.

--- src/scenes/boot.ts
+++ src/scenes/boot.ts
@@ -14,8 +14,6 @@ import { SCENES } from "../util/variables";
 import { UTIL_getPublicDirectoryURLPrefix } from "../util/functions";
 
 // Assets
-import IMG_DEBUG_TILE from "../assets/images/debug_tile.png";
-import IMG_DEBUG_BACKGROUND from "../assets/images/debug_background.jpg";
 
 export default class BootScene extends Phaser.Scene {
 	constructor() {
