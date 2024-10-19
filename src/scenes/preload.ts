a2ca247f3eec8da7c309f76c6a8530912812b753
kadeem254 <kadeemalexander254@gmail.com>
2024-10-19 12:27:13 +0300

    UPDATE: added preload for prototype_tiles, characters and the prototype_level tilemap.

--- src/scenes/preload.ts
+++ src/scenes/preload.ts
@@ -11,8 +11,13 @@
 import Phaser from "phaser";
 
 // Utilities
-import { SCENES } from "../util/variables";
+import { ASSETS, SCENES } from "../util/variables";
 import { UTIL_getPublicDirectoryURLPrefix } from "../util/functions";
+
+// Assets
+import SPRITESHEET_CHARACTERS from "../assets/images/tilemap-characters_packed.png";
+import SPRITESHEET_PROTOTYPE_TILE from "../assets/images/tilemap-prototype_packed.png";
+import TILEMAP_PROTOTYPE_LEVEL from "../assets/tilemaps/prototype-level.json";
 
 export default class PreloadScene extends Phaser.Scene {
 	constructor() {
@@ -24,6 +29,39 @@ export default class PreloadScene extends Phaser.Scene {
 	 */
 	preload() {
 		this.load.setBaseURL(UTIL_getPublicDirectoryURLPrefix());
+
+		// characters and enemies spritesheet
+		this.load.spritesheet(
+			ASSETS.SPRITESHEET.characters,
+			SPRITESHEET_CHARACTERS,
+			{
+				frameWidth: 24,
+				frameHeight: 24,
+				spacing: 0,
+				margin: 0,
+			}
+		);
+
+		// Load these if prototype env is set
+		if (import.meta.env.VITE_SHOW_PROTOTYPE === "true") {
+			// prototype spritesheet
+			this.load.spritesheet(
+				ASSETS.SPRITESHEET.prototype_tiles,
+				SPRITESHEET_PROTOTYPE_TILE,
+				{
+					frameWidth: 18,
+					frameHeight: 18,
+					margin: 0,
+					spacing: 0,
+				}
+			);
+
+			// prototype level tilemap
+			this.load.tilemapTiledJSON(
+				ASSETS.TILEMAP.prototype_level,
+				TILEMAP_PROTOTYPE_LEVEL
+			);
+		}
 	}
 
 	/**
