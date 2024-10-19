1fe760dcb0d19231621c437fb4d01c0f21f5105a
kadeem254 <kadeemalexander254@gmail.com>
2024-10-19 13:42:36 +0300

    ADDED: function to visusalize collision layers for tilemaps.

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,5 +1,42 @@
 import { GAME_ELEMENT } from "./variables";
 
+export function DEBUG_show_tilemap_layer_collisions(
+	scene: Phaser.Scene,
+	tilemapLayer: Phaser.Tilemaps.TilemapLayer,
+	{
+		lineWidth = 1,
+		color = 0xff0000,
+		alpha = 1,
+	}: {
+		lineWidth?: number;
+		color?: number;
+		alpha?: number;
+	},
+	collidingTileColor: Phaser.Display.Color = new Phaser.Display.Color(
+		237,
+		145,
+		64,
+		100
+	),
+	nonCollidingTileColor: Phaser.Display.Color | null = null,
+	faceColor: Phaser.Display.Color = new Phaser.Display.Color(0, 255, 0, 200)
+) {
+	// create graphics
+	const graphics = scene.add.graphics();
+
+	// set oultine
+	graphics.lineStyle(lineWidth, color, alpha);
+
+	// render collision boxes for the collision layer
+	tilemapLayer.renderDebug(graphics, {
+		tileColor: nonCollidingTileColor,
+		collidingTileColor,
+		faceColor,
+	});
+
+	return;
+}
+
 /**
  * Returns a reference to the element that is to house the game, if the element
  * exists within the DOM it will be returned, otherwise it will return null. This
